1487f704037723a5bf956849c5ddb18cce9956d6
uditagarwal29 <uditagarwal1999@gmail.com>
2022-02-27 19:50:32 +0530

    implemented comment delete feature

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,3 +1,4 @@
+const { redirect } = require('express/lib/response')
 const Comment = require('../models/comment')
 const Post = require('../models/post')
 
@@ -18,6 +19,23 @@ module.exports.create = function (req, res) {
                 post.save(); //to save to db from memory
                 res.redirect('/');
             })
+        }
+    })
+}
+
+module.exports.destroy = function (req, res) {
+    Comment.findById(req.params.id, function (err, comment) {
+        //verifying if this comment was made by the logged in user
+        if ((comment.user = req.user.id) || (comment.post.user == req.user.id)) {
+            //saving postId of comment to remove it from comments array in post
+            let postId = comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, function (err, post) {
+                return res.redirect('back');
+            })
+        } else {
+            return res.redirect('back');
         }
     })
 }
