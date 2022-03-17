944de87369d506f2c04a205b808b74f1cbf99d70
uditagarwal29 <uditagarwal1999@gmail.com>
2022-03-17 12:21:38 +0530

    deleting post by using api calls - unauthorized

--- controllers/api/v2/posts_api.js
+++ controllers/api/v2/posts_api.js
@@ -1,6 +1,40 @@
-module.exports.index = function (req, res) {
+const Post = require('../../../models/post')
+const Comment = require('../../../models/comment')
+
+module.exports.index = async function (req, res) {
+    let posts = await Post.find({})
+        .sort('-createdAt')  // sorts posts by latest on top and oldest at bottom
+        .populate('user')
+        .populate({
+            path: 'comments',
+            //further populating comments path to get user who made the comment
+            populate: {
+                path: 'user'
+            },
+            options: { sort: { 'createdAt': -1 } } // sort comments by latest
+        })
     return res.json(200, {
         message: "All Posts : ",
-        posts: []
+        posts: posts
     })
 };
+
+module.exports.destroy = async function (req, res) {
+
+    try {
+        let post = await Post.findById(req.params.id)
+        // if (post.user == req.user.id) {
+        post.remove();
+        await Comment.deleteMany({ post: req.params.id });
+        return res.json(200, {
+            message: "Post deleted"
+        });
+        // } else {
+        //     return res.redirect('back');
+        // }
+    } catch (err) {
+        return res.json(500, {
+            message: "Server Error"
+        });
+    }
+}
