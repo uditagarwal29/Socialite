4100ac623df4816f23c13a9714f4aef741e2618b
uditagarwal29 <uditagarwal1999@gmail.com>
2023-06-28 23:06:23 +0530

    kue error catcher

--- config/kue.js
+++ config/kue.js
@@ -1,3 +1,15 @@
 const kue = require('kue');
+const redis = require('redis');
+
+kue.redis.createClient = function () {
+    var client = redis.createClient();
+
+    client.on("error", function (err) {
+        console.log("trolllolo");
+    });
+    return client;
+};
+
 const queue = kue.createQueue();
+
 module.exports = queue; 
